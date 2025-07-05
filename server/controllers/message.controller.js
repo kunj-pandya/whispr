ac383122c33bc31ed5b711637eaf9d63422dc317
kunj-pandya <kunjpandya998@gmail.com>
2025-07-05 15:20:40 +0530

    Add getMessages controller to retrieve user chat history

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.middleware.js"
 import { User } from "../models/user.model.js";
+import { Message } from "../models/message.model.js";
 
 export const getAllUsers = catchAsyncError(async (req, res, next) => {
     const user = req.user;
@@ -10,5 +11,26 @@ export const getAllUsers = catchAsyncError(async (req, res, next) => {
     });
 });
 
-export const getMessages = catchAsyncError(async (req, res, next) => { });
+export const getMessages = catchAsyncError(async (req, res, next) => {
+    const receiverId = req.params.id;
+    const myId = req.user._id;
+    const receiver = await User.findById(receiverId);
+    if (!receiver) {
+        return res.status(400).json({
+            success: false,
+            message: "Receiver ID Invalid.",
+        });
+    }
+    const messages = await Message.find({
+        $or: [
+            { senderId: myId, receiverId: receiverId },
+            { senderId: receiverId, receiverId: myId }
+        ],
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json({
+        success:true,
+        messages,
+    });
+});
 export const sendMessage = catchAsyncError(async (req, res, next) => { });
