ff00ce580040d2bb323d433d1701c229842775fa
kunj-pandya <kunjpandya998@gmail.com>
2025-07-07 19:28:28 +0530

    Wrap App component with Redux provider in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import store from "./store/store.js";
 
+
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
